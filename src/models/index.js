b9e40fb0807269f9b38e20286d8d447f28a5f641
Diamondbanana420 <benjamesbradshaw22@gmail.com>
2026-02-08 19:01:06 +1000

    Combined backend + V9 frontend

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+const Product = require('./Product');
+const Order = require('./Order');
+const Supplier = require('./Supplier');
+const PipelineRun = require('./PipelineRun');
+const Analytics = require('./Analytics');
+const RecommendationSession = require('./RecommendationSession');
+const SupportTicket = require('./SupportTicket');
+
+module.exports = { Product, Order, Supplier, PipelineRun, Analytics, RecommendationSession, SupportTicket };
